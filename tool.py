d1c1e7bdfa399e2ad69fd1835ea7e386c2697498
Charlie Zhang <sunnypig2002@gmail.com>
2017-09-28 01:35:34 +0800

    finish music corpus

--- tool.py
+++ tool.py
@@ -1,56 +1,87 @@
 import os
 
 import numpy as np
-from sqlalchemy import (
-    Column,
-    Integer,
-    String,
-)
-from sqlalchemy.ext.declarative import declarative_base
+import pandas as pd
+
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.neighbors.classification import (
+    KNeighborsClassifier,
+    RadiusNeighborsClassifier
+)
 
 from support import preprocess_audio
 from model import MusicTaggerCRNN
 
 
-class Comparator(object):
+class Parser(object):
 
     def __init__(self, model):
         self.model = model
 
+    def get_features(self, file):
+        melgram = preprocess_audio(file)
+        melgrams = np.expand_dims(melgram, axis=0)
+        features = self.model.predict(melgrams)
+        return features.reshape(-1)
+
+
+class Comparator(Parser):
+
     def get_similarity(self, file_1, file_2):
-        melgram_1 = preprocess_audio(file_1)
-        melgrams_1 = np.expand_dims(melgram_1, axis=0)
-        preds_1 = self.model.predict(melgrams_1)
-
-        melgram_2 = preprocess_audio(file_2)
-        melgrams_2 = np.expand_dims(melgram_2, axis=0)
-        preds_2 = self.model.predict(melgrams_2)
-
-        return cosine_similarity(preds_1, preds_2)
+        return cosine_similarity(
+            self.get_features(file_1),
+            self.get_features(file_2)
+        )
 
 
-class MusicCorpus(object):
+class MusicCorpus(Parser):
 
-    class Item(declarative_base()):
-        __tablename__ = "song_feature"
-        file_path = Column(String(256), primary_key=True)
-        deep_features = Column(String(512), index=True)
+    def __init__(self, model):
+        super().__init__(model)
+        self.corpus = pd.DataFrame(
+            columns=["path", "features"])
+        self.classifier = RadiusNeighborsClassifier()
 
-    def __init__(self, model, database):
-        self.model = model
+    def scan(self, path):
+        path = os.path.abspath(path)
+        len_orig = len(self.corpus)
+        for dir_name, _, file_names in os.walk(path):
+            for file_name in file_names:
+                file_path = os.path.join(dir_name, file_name)
+                print("parsing file:", file_path)
+                self.corpus.loc[len(self.corpus)] = [
+                    file_path, self.get_features(file_path)]
 
-    def append(self, path):
-        pass
+        len_inc = len(self.corpus) - len_orig
+        if len_inc:
+            print("fitting to classifier with", len_inc, "files")
+            self.classifier.fit(
+                self.corpus["features"][len_orig:].tolist(),
+                np.arange(len(self.corpus))
+            )
 
-    def get_nearest_neighbours(self, num, ):
-        pass
+    def find_similar_audio(self, radius=2.0):
+        similar_group = []
+        for features in self.corpus["features"]:
+            neighbours, = self.classifier.radius_neighbors(
+                [features,], radius=radius, return_distance=False)
+            if len(neighbours) > 1:
+                similar_group.append(
+                    [self.corpus["path"][n] for n in neighbours])
+        return similar_group
 
 
 if __name__ == '__main__':
     # for comparator test
-    c = Comparator(MusicTaggerCRNN(include_top=False))
-    # for file name
-    s = c.get_similarity("data/mandy.mp3", "data/mandy.wma")
+    m = MusicTaggerCRNN(include_top=False)
+    # c = Comparator(m)
+    # # for file name
+    # s = c.get_similarity("data/mandy.mp3", "data/mandy.wma")
+    # print("=======================================")
+    # print("similarity:", s)
+
+    # for corpus test
+    c = MusicCorpus(m)
+    c.scan("data")
     print("=======================================")
-    print("similarity:", s)
+    print(c.find_similar_audio(radius=1.0))
